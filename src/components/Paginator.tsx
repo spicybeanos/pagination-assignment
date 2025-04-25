cdebd8377dcf9ae79770ed7ad30b96570d57aa35
Aryan D <aryan.d.dalal@gmail.com>
2025-04-26 01:01:46 +0530

    added next an prev btn

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -52,43 +52,49 @@ export default function Paginator({ posts }: { posts: APIEvent[] }) {
                     }
                 </div>
 
-                {
-                    page <= 3 &&
-                    <div className="p-3 flex flex-row">
-                        <button onClick={() => { setPage(0) }} className={(page != 0 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>1</button>
-                        <button onClick={() => { setPage(1) }} className={(page != 1 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>2</button>
-                        <button onClick={() => { setPage(2) }} className={(page != 2 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>3</button>
-                        <button onClick={() => { setPage(3) }} className={(page != 3 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>4</button>
-                        <button onClick={() => { setPage(4) }} className={(page != 4 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>5</button>
-                        <div className="text-[#E82677] ">...</div>
-                        <button onClick={() => { setPage(lastPage - 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{Math.ceil(posts.length / itemsPerPage)}</button>
-                    </div>
-                }
-                {
-                    page > 3 && page <= lastPage - 3 &&
-                    <div className="p-3 flex flex-row">
-                        <button className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">1</button>
-                        <div className="text-[#E82677] ">...</div>
-                        <button onClick={() => { setPage(page - 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{page}</button>
-                        <button onClick={() => { setPage(page) }} className="bg-[#E82677] text-white shadow-xl/30 cursor-pointer text-2xl rounded-full px-[9px] m-1">{page + 1}</button>
-                        <button onClick={() => { setPage(page + 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{page + 2}</button>
-                        <div className="text-[#E82677] ">...</div>
-                        <button className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{lastPage}</button>
-                    </div>
-                }
+                <div className="flex flex-row">
+                    <button onClick={() => setPage(Math.max(page - 1, 0))} className="cursor-pointer rounded-lg"><img src='/left.svg' /></button>
+                    {
+                        page <= 3 &&
+                        <div className="p-3 flex flex-row">
+                            <button onClick={() => { setPage(0) }} className={(page != 0 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>1</button>
+                            <button onClick={() => { setPage(1) }} className={(page != 1 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>2</button>
+                            <button onClick={() => { setPage(2) }} className={(page != 2 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>3</button>
+                            <button onClick={() => { setPage(3) }} className={(page != 3 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>4</button>
+                            <button onClick={() => { setPage(4) }} className={(page != 4 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>5</button>
+                            <div className="text-[#E82677] ">...</div>
+                            <button onClick={() => { setPage(lastPage - 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{Math.ceil(posts.length / itemsPerPage)}</button>
+                        </div>
+                    }
+                    {
+                        page > 3 && page <= lastPage - 3 &&
+                        <div className="p-3 flex flex-row">
+                            <button className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">1</button>
+                            <div className="text-[#E82677] ">...</div>
+                            <button onClick={() => { setPage(page - 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{page}</button>
+                            <button onClick={() => { setPage(page) }} className="bg-[#E82677] text-white shadow-xl/30 cursor-pointer text-2xl rounded-full px-[9px] m-1">{page + 1}</button>
+                            <button onClick={() => { setPage(page + 1) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{page + 2}</button>
+                            <div className="text-[#E82677] ">...</div>
+                            <button className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">{lastPage}</button>
+                        </div>
+                    }
 
-                {
-                    page > lastPage - 3 &&
-                    <div className="p-3 flex flex-row">
-                        <button onClick={() => { setPage(0) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">1</button>
-                        <div className="text-[#E82677] ">...</div>
-                        <button onClick={() => { setPage(lastPage - 5) }} className={(page != lastPage - 5 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 4}</button>
-                        <button onClick={() => { setPage(lastPage - 4) }} className={(page != lastPage - 4 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 3}</button>
-                        <button onClick={() => { setPage(lastPage - 3) }} className={(page != lastPage - 3 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 2}</button>
-                        <button onClick={() => { setPage(lastPage - 2) }} className={(page != lastPage - 2 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 1}</button>
-                        <button onClick={() => { setPage(lastPage - 1) }} className={(page != lastPage - 1 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage}</button>
-                    </div>
-                }
+                    {
+                        page > lastPage - 3 &&
+                        <div className="p-3 flex flex-row">
+                            <button onClick={() => { setPage(0) }} className="text-[#E82677] cursor-pointer text-2xl rounded-full px-[9px] m-1">1</button>
+                            <div className="text-[#E82677] ">...</div>
+                            <button onClick={() => { setPage(lastPage - 5) }} className={(page != lastPage - 5 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 4}</button>
+                            <button onClick={() => { setPage(lastPage - 4) }} className={(page != lastPage - 4 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 3}</button>
+                            <button onClick={() => { setPage(lastPage - 3) }} className={(page != lastPage - 3 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 2}</button>
+                            <button onClick={() => { setPage(lastPage - 2) }} className={(page != lastPage - 2 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage - 1}</button>
+                            <button onClick={() => { setPage(lastPage - 1) }} className={(page != lastPage - 1 ? "text-[#E82677] " : "bg-[#E82677] text-white shadow-xl/30 ") + "cursor-pointer text-2xl rounded-full px-[9px] m-1"}>{lastPage}</button>
+                        </div>
+                    }
+
+                    <button onClick={() => setPage(Math.min(page + 1, lastPage - 1))} className="cursor-pointer border-[#E82677] rounded-lg"><img src='/right.svg' /></button>
+                </div>
+
 
             </div>
 
